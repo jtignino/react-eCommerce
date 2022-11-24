38bf713767050e8c9aca1be9678446df85ba87d5
jtignino <jonathan.jat@hotmail.com>
2022-11-24 09:13:42 -0300

    Mejoras de estilos con CSS.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,8 @@ const App = () => {
   return (
     <>
       <Navbar/>
-      
-      {/* <p className='container-xxl text-center mb-5 parrafoUno'>¡Hola, soy Jesi! Mi trabajo es ayudarte a construir una marca que deje huella. Una marca que comunique con pasión y amor. Estamos hechos de emociones, y los negocios también. Por eso ayudo a las empresas a traspasar la pantalla llegando al corazón del público. ¿Para qué? Para que cada persona que te conozca vea más allá de lo que vendés, para que ame tanto tu trabajo que el precio que tenga sea irrelevante para ellos.</p> */}
       <SectionPrincipal/>
-      <ItemListContainer greeting={"Este es un 'ItemListContainer'. Acá se visualizarán los productos."}/>
+      <ItemListContainer greeting={"Este es un 'ItemListContainer' vacío. Acá se visualizarán los productos."}/>
     </>
   );
 }
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,11 +2,6 @@ import './CartWidget.css';
 const CartWidget = () => {
     return (
         <ul className="navbar-nav ms-auto align-items-center">
-            {/* <button className='btn btn-lg position-relative p-2'>
-                <i className="fa-solid fa-cart-shopping colorIcon p-0"></i>
-                <span class="position-absolute top-0 start-100 translate-middle contadorCarrito">0</span>
-            </button> */}
-
             <div className='position-relative'>
                 <button className='btn btn-lg position-relative- p-2'>
                     <i className="fa-solid fa-cart-shopping colorIcon p-0"></i>   
