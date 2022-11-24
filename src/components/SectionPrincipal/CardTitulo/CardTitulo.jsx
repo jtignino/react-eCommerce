b3f1db78efd0f7c07e57ffcc153a16034e356293
jtignino <jonathan.jat@hotmail.com>
2022-11-24 09:38:15 -0300

    Mejoras de estilos con CSS.

--- src/components/SectionPrincipal/CardTitulo/CardTitulo.jsx
+++ src/components/SectionPrincipal/CardTitulo/CardTitulo.jsx
@@ -2,7 +2,7 @@ import './CardTitulo.css';
 
 const CardTitulo = () => {
     return (
-        <div className='d-flex flex-column'>
+        <div className='d-flex flex-column w-50'>
             <h1 className='text-white titulo'>Piensa Digital</h1>
             <h2 className='text-white subtitulo'>Publicidad & Marketing Digital.</h2>
             <p className='text-white text-left my-4 parrafoUno'>¡Hola, soy Jesi! Mi trabajo es ayudarte a construir una marca que deje huella. Una marca que comunique con pasión y amor. Estamos hechos de emociones, y los negocios también. Por eso ayudo a las empresas a traspasar la pantalla llegando al corazón del público. ¿Para qué? Para que cada persona que te conozca vea más allá de lo que vendés, para que ame tanto tu trabajo que el precio que tenga sea irrelevante para ellos.</p>
--- src/components/SectionPrincipal/SectionPrincipal.jsx
+++ src/components/SectionPrincipal/SectionPrincipal.jsx
@@ -4,8 +4,9 @@ import './SectionPrincipal.css';
 const SectionPrincipal = () => {
     return (
         <div className='position-relative divPrincipal'>
-            <section className='container-xl d-flex flex-column w-100 sectionPrincipal justify-content-center'>
+            <section className='container-xl d-flex position-relative-- flex-row w-100 sectionPrincipal justify-content-between align-items-center'>
                 <CardTitulo/>
+                <div className='w-50-- bgImage'></div>
             </section>
             <div className='position-absolute bottom-0 start-0 svgContainer'>
                 <svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: "100%", width: "100%",}}><path d="M-2.54,66.60 C149.83,-42.92 285.83,141.60 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style={{stroke: "none", fill: "#ffffff",}}></path></svg>
